647d9239893489075b28e6c842a3355f365383d8
gaurav malik <gauravmalik@gauravs-MacBook-Pro.local>
2017-09-21 20:18:06 -0600

    updated

--- src/main/java/com/example/demo/WelcomeController.java
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-	@RequestMapping("/helloworld")
+	@RequestMapping("/")
 	public String welcome() {
-		System.out.println("calling helloworld api /helloworld ");
+		System.out.println("calling helloworld api");
 		return "Hello World !";
 	}
 
